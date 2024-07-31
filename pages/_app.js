355815b63d9f155d84e164002e5795dc6e4b7aca
Must-be-Ash <arash.nouruzi@yahoo.com>
2024-07-30 22:44:34 -0700

    Implemented game improvements and mobile support

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,16 @@
 import dynamic from 'next/dynamic'
+import { Analytics } from "@vercel/analytics/react"
 
 const DynamicHome = dynamic(() => import('./index'), { ssr: false })
 
 function MyApp({ Component, pageProps }) {
-  return <DynamicHome {...pageProps} />
+  return (
+  <>
+  <Analytics />
+  <DynamicHome {...pageProps} />
+  </>
+  )
+
 }
 
 export default MyApp
